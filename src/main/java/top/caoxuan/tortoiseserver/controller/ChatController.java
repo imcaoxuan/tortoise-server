19aef4c694dffbca73b1a90f886fb037af815cc2
CaoXuan <c17762500354@gmail.com>
2020-10-16 15:26:05 +0800

    2020/10/16 15:25

--- src/main/java/top/caoxuan/tortoiseserver/controller/ChatController.java
+++ src/main/java/top/caoxuan/tortoiseserver/controller/ChatController.java
@@ -1,7 +1,12 @@
 package top.caoxuan.tortoiseserver.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * @author CX
@@ -12,7 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ChatController {
 
     @RequestMapping("/chat")
-    public String chat() {
+    public String chat(Model model) throws UnknownHostException {
+        InetAddress inetAddress = Inet4Address.getLocalHost();
+        String ip = inetAddress.getHostAddress();
+        model.addAttribute("ip", ip);
         return "multiplayer_sport";
     }
 }
